8f5d8c9311771bb1798d7727375f699fbbf2e673
Vinny <marcos.v@rgsistemas.com.br>
2016-10-17 16:40:58 -0300

    refactor routes + vuex + include axios

---
+++ src/app/dashboard/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="page-header row">
+    <h2>Vue - 2.0</h2>
+    <!-- <pre>
+      {{ }}
+    </pre> -->
+  </header>
+</template>
+
+<script>
+import http from 'src/http'
+export default {
+  mounted () {
+    http.get('http://pokeapi.co/api/v2/pokemon/1').then(response => response.data)
+    .then(data => {
+      console.log(data)
+    })
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-
 import router from './router/index.js'
+import store from './vuex'
 
 /* eslint-disable no-new */
 new Vue({
   router,
+  store,
   el: '#app',
   render: h => h(App)
 })
